043f875de69be7a31f9c58d8a6c3f273f56c6b88
Henrik Christoffersen <hc@hench.dk>
2020-01-12 14:33:39 +0100

    did some more work on authentification

--- Authentification/app.js
+++ Authentification/app.js
@@ -1,23 +1,35 @@
-var express                     = require('express'),
-    mongoose                    = require('mongoose'),
-    passport                    = require('passport'),
-    bodyParser                  = require('body-parser'),
-    User                        = require('./models/user')
-    LocalStrategy               = require('passport-local'),
-    passportLocalMongoose       = require('passport-local-mongoose');
+var express               = require("express"),
+    mongoose              = require("mongoose"),
+    passport              = require("passport"),
+    bodyParser            = require("body-parser"),
+    User                  = require("./models/user"),
+    LocalStrategy         = require("passport-local"),
+    passportLocalMongoose = require("passport-local-mongoose")
 
 // CONFIG EXPRESS
 var app = express();
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
 
-// CONFIG EXPRESS TO USE PASSPORT
-app.use(require('express-session')({
-    secret: "Rusty is the cutest dog in the world!",
+
+
+/*  
+    // CONFIG PASSPORT AUTH
+    // create a session middleware
+    // the middleware is run on the server before returning anything to the user
+    // the secret is used to compute the session hash and prevent tampering
+    // LocalStrategy is one of many ways for user to authenticate him/herself
+    // serializeUser determines which data of the user object should be stored in the session
+*/
+
+app.use(require("express-session")({
+    secret: "Rusty is the best and cutest dog in the world",
     resave: false,
-    saveUnitialized: false
+    saveUninitialized: false
 }));
 app.use(passport.initialize());
 app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
@@ -33,25 +45,63 @@ mongoose.connect('mongodb+srv://hench:6qUVx4U8kSe2YPVo@cluster0-d6nft.azure.mong
 })
 
 
+//============
 // ROUTES
-app.get('/', function(req, res) {
-    res.render('home');
-})
+//============
 
-app.get('/secret', function(req, res) {
-    res.render('secret');
-})
+app.get("/", function(req, res){
+    res.render("home");
+});
 
-// AUTH ROUTES
-app.get('/register', function(req, res){
-    res.render('register');
-})
+app.get("/secret", isLoggedIn, function(req, res){
+   res.render("secret"); 
+});
 
-app.post('/register', function(req, res){
-    res.send('REGISTER POST ROUTE');
-})
+// Auth Routes
 
-// START THE SERVER
+//show sign up form
+app.get("/register", function(req, res){
+   res.render("register"); 
+});
+//handling user sign up
+app.post("/register", function(req, res){
+    User.register(new User({username: req.body.username}), req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.render('register');
+        }
+        passport.authenticate("local")(req, res, function(){
+           res.redirect("/secret");
+        });
+    });
+});
+
+// LOGIN ROUTES
+//render login form
+app.get("/login", function(req, res){
+   res.render("login"); 
+});
+//login logic
+//middleware
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/secret",
+    failureRedirect: "/login"
+}) ,function(req, res){
+});
+
+app.get("/logout", function(req, res){
+    req.logout();
+    res.redirect("/");
+});
+
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
+
 app.listen(8080, function(){
-    console.log('started listening on port 8080');
+    console.log("server started.......");
 })
